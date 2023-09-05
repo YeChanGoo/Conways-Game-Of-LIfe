0a055b32b2b34fbdab62119353f8716623635d7d
YeChanGoo <126903033+YeChanGoo@users.noreply.github.com>
2023-09-05 14:54:28 -0400

    feat: added hooks file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer: React.FC = () => {
+interface FooterProps {
+  onRandom: () => void;
+  computeNextGeneration: () => void;
+  start: boolean;
+  onStartWithRandomGrid: () => void;
+  onClear: () => void;
+}
+
+const Footer: React.FC<FooterProps> = ({
+  onRandom,
+  start,
+  onStartWithRandomGrid,
+  onClear,
+}) => {
   return (
     <div className='footer'>
-      <button>Randomize</button>
-      <button>Clear Board</button>
-      <button>{"Start"}</button>
+      <button onClick={() => onRandom()}>Random</button>
+      <button onClick={() => onClear()}>Clear Board</button>
+      <button onClick={() => onStartWithRandomGrid()}>
+        {start ? "Stop" : "Start"}
+      </button>
       <button>Increase Speed</button>
       <button>Decrease Speed</button>
     </div>
